a6712471367b3db5ce46a7f7d9fb2862a0fdb652
Fernando Soares <dotfernando@gmail.com>
2017-03-05 21:35:26 -0300

    mais alguns ajustes e por hoje é só 😴

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,23 @@
+$.jribbble.setToken('0cd1ef6639b1a1d539d62e32712fa8568de32815169192bb33ffd7c698ccb64e');
+
+$.jribbble.users('fernando').shots({
+  per_page: 9
+}).then(function(shots) {
+      var html = [];
+
+      shots.forEach(function(shot) {
+        // See the Dribbble docs for all available shot properties.
+        html.push('<li class="shots--shot">');
+        html.push('<a href="' + shot.html_url + '">');
+        html.push('<img src="' + shot.images.normal + '">');
+        html.push('</a></li>');
+      });
+
+      $('.shots').html(html.join(''));
+    });
+
+      // Twitter script.
+
 window.twttr = (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0],
     t = window.twttr || {};
