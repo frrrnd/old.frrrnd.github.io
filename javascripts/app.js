cbf231d1aae5a8c95326c3ecd1ffed04bf255324
Fernando Soares <dotfernando@gmail.com>
2017-03-07 23:24:13 -0300

    um negocinho aqui 💎

--- javascripts/app.js
+++ javascripts/app.js
@@ -16,24 +16,6 @@ $.jribbble.users('fernando').shots({
       $('.shots').html(html.join(''));
     });
 
-      // Twitter script.
-
-window.twttr = (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0],
-    t = window.twttr || {};
-  if (d.getElementById(id)) return t;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://platform.twitter.com/widgets.js";
-  fjs.parentNode.insertBefore(js, fjs);
-
-  t._e = [];
-  t.ready = function(f) {
-    t._e.push(f);
-  };
-
-  return t;
-}(document, "script", "twitter-wjs"));
 
 $('.bt-menu').on('click', function(e) {
   e.preventDefault();
