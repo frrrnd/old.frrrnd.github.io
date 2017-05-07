e972e95925b32aefb7ec09861bb9114845e1d955
Fernando Soares <dotfernando@gmail.com>
2017-05-06 22:56:07 -0300

    ajustes de layout 💃🏻

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,21 +1,3 @@
-$.jribbble.setToken('0cd1ef6639b1a1d539d62e32712fa8568de32815169192bb33ffd7c698ccb64e');
-
-$.jribbble.users('fernando').shots({
-  per_page: 9
-}).then(function(shots) {
-      var html = [];
-
-      shots.forEach(function(shot) {
-        // See the Dribbble docs for all available shot properties.
-        html.push('<li class="shots--shot shots--reveal">');
-        html.push('<a href="' + shot.html_url + '">');
-        html.push('<img src="' + shot.images.normal + '">');
-        html.push('</a></li>');
-      });
-
-      $('.shots').html(html.join(''));
-    });
-
 var list = ["👽", "👾", "🤖", "🎃", "✌", "🤘", "🖖", "⚡", "🔥", "❄", "🍕", "🍔", "🌮", "🍩", "🍺",  "🎸", "🎮", "🚀", "🍗"];
 
 function shuffle(array) {
